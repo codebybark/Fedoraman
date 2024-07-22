bd1345d305048af5d1c03420a715dbe8d334fe73
just BПRK <68547397+codebybark@users.noreply.github.com>
2024-07-22 07:15:22 +0500

    Untested commit 01

---
+++ SetupRespos.sh
@@ -0,0 +1,6 @@
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf config-manager --enable fedora-cisco-openh264
+
+sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
